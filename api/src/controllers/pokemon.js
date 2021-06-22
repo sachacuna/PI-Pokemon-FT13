ad757c337d16f9afd8c9e36f7ce51cb23d57e06d
=sachacuna <=>
2021-06-22 14:48:15 -0300

    create and search components

--- api/src/controllers/pokemon.js
+++ api/src/controllers/pokemon.js
@@ -103,6 +103,7 @@ pokeCtrl.createPokemon = async (req, res, next) => {
         const body = req.body
         const newPoke = { ...body, id: uuidv4() }
         await Pokemon.create(newPoke)
+        //faltaria agregar prop por prop
         res.status(201).json(newPoke)
     }
     catch (error) {
