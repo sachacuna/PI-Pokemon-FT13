9e66bfe72b5d4351f142e8b9b05afe0aaa1c13c0
Sacha Cuña <sachacuna@gmail.com>
2023-12-27 16:33:24 -0600

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,17 +1,7 @@
 const { Router } = require('express');
-//const pokemon = require('./pokemon')
-//const type = require('./type')
-//const { Pokemon, Type } = require('../db')
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-//const {getPokemons, createPokemon, getPokemonsCreated, getPokemon}
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
 router.use('/pokemons', require('./pokemon'))
-//router.use('/pokemon', pokemon)
 router.use('/types', require('./type'))
 
 module.exports = router;
