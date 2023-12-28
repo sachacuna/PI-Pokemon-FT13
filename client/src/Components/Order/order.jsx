b9299f5283b8c1fd010eb8afa366ec441ee998ab
Sacha Cuña <sachacuna@gmail.com>
2023-12-27 20:27:20 -0600

    Update order.jsx

--- client/src/Components/Order/order.jsx
+++ client/src/Components/Order/order.jsx
@@ -23,7 +23,6 @@ export default function Order() {
 
     const [selectedType, setSelectedType] = useState('')
     const [filterType, setFilterType] = useState([])
-    //const [filterType, setFilterType] = useState([])
 
     const pokemons = useSelector(state => state.pokemons)
     const pokemonsAlt = useSelector(state => state.pokemonsAlt)
@@ -128,25 +127,3 @@ export default function Order() {
         </div>
     )
 }
-
-
-    // <option value="normal">Normal</option>
-    //                 <option value="fighting">Fighting</option>
-    //                 <option value="flying">Flying</option>
-    //                 <option value="poison">Poison</option>
-    //                 <option value="ground">Ground</option>
-    //                 <option value="rock">Rock</option>
-    //                 <option value="bug">Bug</option>
-    //                 <option value="ghost">Ghost</option>
-    //                 <option value="steel">Steel</option>
-    //                 <option value="fire">Fire</option>
-    //                 <option value="water">Water</option>
-    //                 <option value="grass">Grass</option>
-    //                 <option value="electric">Electric</option>
-    //                 <option value="psychic">Psychic</option>
-    //                 <option value="ice">Ice</option>
-    //                 <option value="dragon">Dragon</option>
-    //                 <option value="dark">Dark</option>
-    //                 <option value="fairy">Fairy</option>
-    //                 <option value="unknown">Unknown</option>
-    //                 <option value="shadow">Shadow</option>
