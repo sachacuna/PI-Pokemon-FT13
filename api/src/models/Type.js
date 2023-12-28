fe98a032778621593e178ced49d8d0851c742e62
Sacha Cuña <sachacuna@gmail.com>
2023-12-27 18:40:14 -0600

    Update Type.js

--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -2,10 +2,6 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('Type', {
-    /*id: {
-       type: DataTypes.UUID,
-      primaryKey: true,
-    }, */
     name: {
       type: DataTypes.STRING,
     },
